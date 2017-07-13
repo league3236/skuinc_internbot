73862e0f9692cc617140e36ae1f5f1f076a7dba9
kwak <league3236@gmail.com>
2017-07-13 16:29:28 +0900

    2017-07-13-16:32

--- slackbot/calendarunion.py
+++ slackbot/calendarunion.py
@@ -1,19 +1,25 @@
+# -*- coding: utf-8 -*-
 
 from __future__ import print_function
 import httplib2
 import os
 
+from slackclient import SlackClient
 from apiclient import discovery
 from oauth2client import client
 from oauth2client import tools
 from oauth2client import file
 from httplib2 import Http
 from oauth2client.file import Storage
-
+from slacker import Slacker
 
 import datetime
 #library???
 
+slack = Slacker(os.environ.get("SLACK_BOT_TOKEN"))
+
+def post_to_channel(message):
+    slack.chat.post_message('testtest',message,as_user=True)
 
 try:
     import argparse
@@ -54,7 +60,7 @@ def get_credentials():
             credentials = tools.run_flow(flow, store, flags)
         else: # Needed only for compatibility with Python 2.6
             credentials = tools.run(flow, store)
-        print('Storing credentials to ' + credential_path)
+        answer='Storing credentials to ' + credential_path
     return credentials
 
 def get_AllList():
@@ -75,15 +81,15 @@ def get_AllList():
     events = eventsResult.get('items', [])
 
     #now1 = datetime.datetime.now()
-    print('All event dateTime and eventName:')
+    answer='All event dateTime and eventName:'
     #nowDate = now.strftime('%Y-%m-%d')
     #print(nowDate)  # 2015-04-19
 
     if not events:
-        print('No upcoming events found.')
+        answer='No upcoming events found.'
     for event in events:
         start = event['start'].get('dateTime', event['start'].get('date'))
-        print('DAY:'+start.split('+')[0].split('T')[0],'Time:'+start.split('+')[0].split('T')[1].split(':')[0]+(':')+start.split(':')[1],'Summary:'+event['summary'])
+        answer='DAY:'+start.split('+')[0].split('T')[0],'Time:'+start.split('+')[0].split('T')[1].split(':')[0]+(':')+start.split(':')[1],'Summary:'+event['summary']
     return 0
 def get_TodayList():
     """Shows basic usage of the Google Calendar API.
@@ -104,17 +110,55 @@ def get_TodayList():
 
     now=datetime.datetime.now()
     nowDate = now.strftime('%Y-%m-%d')
-    print('Today event dateTime and eventName:')
-
+    answer='Today event dateTime and eventName:'
+    post_to_channel(answer)
     if not events:
-        print('No upcoming events found.')
+        answer ='No upcoming events found.'
+        post_to_channel(answer)
     for event in events:
         start = event['start'].get('dateTime')
         end = event['end'].get('dateTime')
         today = start.split('T')[0]
         if today == nowDate:
-           print("Time:"+start.split('+')[0].split('T')[1].split(':')[0]+(':')+start.split(':')[1]+"~"+end.split('+')[0].split('T')[1].split(':')[0]+(':')+start.split(':')[1],"summary:"+event['summary'])
-           #print(event['id'])
+            answer ="Time:"+start.split('+')[0].split('T')[1].split(':')[0]+(':')+start.split(':')[1]+"~"+end.split('+')[0].split('T')[1].split(':')[0]+(':')+start.split(':')[1],"summary:"+event['summary']
+            post_to_channel(answer)
+            #print(event['id'])
+    return 0
+
+def get_TomorrowList():
+    """Shows basic usage of the Google Calendar API.
+
+            Creates a Google Calendar API service object and outputs a list of the next
+            10 events on the user's calendar.
+            """
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build('calendar', 'v3', http=http)
+
+    now1 = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
+    # print('Getting the upcoming 10 events')
+    eventsResult = service.events().list(
+        calendarId=CA_NAME, timeMin=now1, maxResults=10, singleEvents=True,
+        orderBy='startTime').execute()
+    events = eventsResult.get('items', [])
+
+    now = datetime.datetime.now()
+    tomorrowDate = str(now + datetime.timedelta(days=1)).split(' ')[0]
+
+    answer='Tomorrow event dateTime and eventName:'
+    post_to_channel(answer)
+    if not events:
+        answer='No upcoming events found.'
+        post_to_channel(answer)
+    for event in events:
+        start = event['start'].get('dateTime')
+        end = event['end'].get('dateTime')
+        tomorrow = start.split('T')[0]
+
+        if tomorrow == tomorrowDate:
+            answer = "Time:" + start.split('+')[0].split('T')[1].split(':')[0] + (':') + start.split(':')[1] + "~"+end.split('+')[0].split('T')[1].split(':')[0] + (':') + start.split(':')[1],"summary:" + event['summary']
+            post_to_channel(answer)
+            # print(event['id'])
     return 0
 
 def insert_Event(EventName,StartTime,EndTime):
@@ -143,14 +187,14 @@ def insert_Event(EventName,StartTime,EndTime):
 
     e = GCAL.events().insert(calendarId=CA_NAME, sendNotifications=True, body=EVENT).execute()
 
-    print('''*** %r event added:
-        Start: %s
-        End:   %s''' % (e['summary'].encode('utf-8'),
-                        e['start']['dateTime'].split('T')[0] + " " + e['start']['dateTime'].split('T')[1].split('+')[0],
-                        e['end']['dateTime'].split('T')[0] + " " + e['end']['dateTime'].split('T')[1].split('+')[0]))
+    answer='''*** %r event added:
+           Start: %s
+           End:   %s''' % (e['summary'].encode('utf-8'),
+                           e['start']['dateTime'].split('T')[0] + " " + e['start']['dateTime'].split('+')[0].split('T')[1].split(':')[0]+(':')+e['start']['dateTime'].split(':')[1],
+                           e['end']['dateTime'].split('T')[0] + " " + e['end']['dateTime'].split('+')[0].split('T')[1].split(':')[0]+(':')+e['start']['dateTime'].split(':')[1])
 
     return 0
-
+#print('DAY:'+start.split('+')[0].split('T')[0],'Time:'+start.split('+')[0].split('T')[1].split(':')[0]+(':')+start.split(':')[1],'Summary:'+event['summary'])
 
 def convert(date,time):
     now = datetime.datetime.now()
@@ -170,7 +214,7 @@ def delete_Event(Event_Id):
         orderBy='startTime').execute()
     events = eventsResult.get('items', [])
 
-    print('Events Delete:')
+    answer='Events Delete:'
     service.events().delete(calendarId=CA_NAME, eventId=Event_Id).execute()
 
     return 0
@@ -198,20 +242,23 @@ def get_EventID(D_Day,E_Summary):
     return event_id
 
 def main():
+    print('실행되는중')
     #command("event all")
     get_AllList()
     #command("event today")
     get_TodayList()
+    #command("Tomorrow")
+    get_TomorrowList()
 
     #insert
     #command("insert 15 13:00-14:00 summary")
     #((^\D+)\s(\d+)\s((\d+):(\d+))(-|~)((\d+):(\d+))\s(\D+))
 
 
-    #E_Day=''
-    #s_Time=''
-    #e_Time=''
-    #event_Name = ''
+    #E_Day='14'
+    #s_Time='13:00'
+    #e_Time='14:00'
+    #event_Name = 'test'
     #s_dateTime = convert(E_Day,s_Time)
     #e_dateTime = convert(E_Day,e_Time)
     #insert_Event(event_Name,s_dateTime,e_dateTime)
--- slackbot/dailybot.py
+++ slackbot/dailybot.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 
+import calendarunion
 import os
 import time
 import datetime
 import json
 import urllib2
 from slackclient import SlackClient
+from slacker import Slacker
 
 
 # starterbot's ID as an environment variable
@@ -19,8 +21,12 @@ EXAMPLE_COMMAND2 = "tomorrow"
 BL = True;
 
 # instantiate Slack & Twilio clients
+
+slack = Slacker(os.environ.get('SLACK_BOT_TOKEN'))
 slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
 
+def post_to_channel(message):
+    slack.chat.post_message('testtest', message, as_user=True)
 
 def parse_slack(msg):
     output_list = msg
@@ -68,12 +74,13 @@ def slack_answer(txt):
     if txt == EXAMPLE_COMMAND1:
         answer = "안녕하세요! 일정봇입니다."
     elif txt == EXAMPLE_COMMAND2:
-	payload = {"text": "내일일정 알림!!!"}
-	url = "https://hooks.slack.com/services/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"
-	req = urllib2.Request(url)
-	req.add_header('Content-Type', 'application/json')
-	urllib2.urlopen(req, json.dumps(payload))
-	return None
+        calendarunion.get_TomorrowList() 
+	#payload = {"text": "내일일정 알림!!!"}
+	#url = "https://hooks.slack.com/services/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"
+	#req = urllib2.Request(url)
+	#req.add_header('Content-Type', 'application/json')
+	#urllib2.urlopen(req, json.dumps(payload))
+	#return None
     else:
         return None
 
